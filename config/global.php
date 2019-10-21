ec7d0a6106bf14cca0312b50cc5e1bc164f5f7f1
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-10-20 23:04:12 -0500

    v017 conexion de la base de datos mysql y php

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+// IP de la pc servicor de base de datos
+define("DB_HOST", "localhost");
+
+//Nombre d ela base de datos
+define("DB_NAME", "dbsistema");
+
+//Usuario de la base de datos
+define("DB_USERNAME", "root");
+
+//Contraseña del usuario de la base de datos
+define("DB_PASSWORD", "");
+
+//definimos la codificación de los caracteres
+define("DB_ENCODE", "utf8");
+
+//Definimos una constante como nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+ ?>
