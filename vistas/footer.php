fc64efc00ac85d213602adfc3a7d04f48b6c7a5f
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-10-21 04:16:31 -0500

    modificacion de plantilla html y bootstrap

---
+++ vistas/footer.php
@@ -0,0 +1,14 @@
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 2.3.0
+        </div>
+        <strong>Copyright &copy; 2019 <a href="www.incanatoit.com">Anonymous</a>.</strong> All rights reserved.
+    </footer>    
+    <!-- jQuery 2.1.4 -->
+    <script src="../public/js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../public/js/bootstrap.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../public/js/app.min.js"></script>
+  </body>
+</html>
